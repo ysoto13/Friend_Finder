73e901ece8c5cd9c06dc5903d9dd0361d90d21c1
Yadira Soto <lunar13heart@hotmail.com>
2019-07-08 18:53:17 -0400

    edited files

--- Independence-egg/App_Friend_Finder/app/routing/apiRoutes.js
+++ Independence-egg/App_Friend_Finder/app/routing/apiRoutes.js
@@ -4,59 +4,59 @@
 // These data sources hold arrays of information on all possible friends
 // ===============================================================================
 
-
+var friendsData = require("../data/friendsData");
+var homeData = require("../data/homeData");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-
+module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
- 
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  app.get("/api/home", function(req, res) {
+    res.json(homeData);
+  });
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (friendsData.length < 5) {
+      friendsData.push(req.body);
+      res.json(true);
+    }
+    else {
+      homeData.push(req.body);
+      res.json(false);
+    }
+  });
 
-    // Note the code here. Our "server" will respond to a user"s survey result
-    // Then compare those results against every user in the database.
-    // It will then calculate the difference between each of the numbers and the user"s numbers.
-    // It will then choose the user with the least differences as the "best friend match."
-    // In the case of multiple users with the same result it will choose the first match.
-    // After the test, it will push the user to the database.
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
 
-    // We will use this object to hold the "best match". We will constantly update it as we
-    // loop through all of the options
- 
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    friendsData.length = 0;
+    homeData.length = 0;
 
-    // Here we take the result of the user"s survey POST and parse it.
-    
-    // This variable will calculate the difference between the user"s scores and the scores of
-    // each user in the database
-    
-
-    // Here we loop through all the friend possibilities in the database.
-    
-      // We then loop through all the scores of each friend
-      
-        // We calculate the difference between the scores and sum them into the totalDifference
-   
-
-      // If the sum of differences is less then the differences of the current "best match"
-
-        // Reset the bestMatch to be the new friend.
-     
-    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-    // the database will always return that the user is the user's best friend).
-
-
-    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
- 
+    res.json({ ok: true });
+  });
+};
